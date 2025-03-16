a14d81aa066ce7b3fe70ba0a2d338d751991eb5c
Custom PC Debrecen <szaszpetya@gmail.com>
2025-03-16 12:53:38 +0100

    Task1 - fix remaining issues - Experiment to run only the problematic test for Edge

--- src/test/java/com/homework/test/automation/tests/ui/RichTextEditorPageTest.java
+++ src/test/java/com/homework/test/automation/tests/ui/RichTextEditorPageTest.java
@@ -1,60 +1,60 @@
-package com.homework.test.automation.tests.ui;
-
-import com.homework.test.automation.config.SpringConfig;
-import com.homework.test.automation.factory.DriverFactory;
-import com.homework.test.automation.pageobjects.editor.RichTextEditorPage;
-import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.WebDriver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-@Slf4j
-@ContextConfiguration(classes = SpringConfig.class)
-public class RichTextEditorPageTest extends AbstractTestNGSpringContextTests {
-
-    @Autowired
-    private DriverFactory driverFactory;
-
-    private WebDriver driver;
-
-    private RichTextEditorPage richTextEditorPage;
-
-    @BeforeClass
-    public void setup() {
-        driver = driverFactory.createAndGetWebDriver();
-        richTextEditorPage = new RichTextEditorPage(driver);
-    }
-
-    @AfterClass
-    public void tearDown() {
-        log.info("Shutting down the driver.");
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    @Test
-    public void testRichTextEditor() {
-        // Given
-        final String inputTextAutomation = "Automation ";
-        final String inputTextTest = "Test";
-        final String inputTextExample = " Example";
-
-        // When - Then
-        richTextEditorPage.openPage()
-                .toggleBold()
-                .enterText(inputTextAutomation)
-                .toggleBold()
-                .toggleUnderline()
-                .enterText(inputTextTest)
-                .toggleUnderline()
-                .enterText(inputTextExample)
-                .assertTypedTextContainsBold(inputTextAutomation)
-                .assertTypedTextContainsUnderlined(inputTextTest)
-                .assertTypedTextContainsPlain(inputTextExample);
-    }
-}
+//package com.homework.test.automation.tests.ui;
+//
+//import com.homework.test.automation.config.SpringConfig;
+//import com.homework.test.automation.factory.DriverFactory;
+//import com.homework.test.automation.pageobjects.editor.RichTextEditorPage;
+//import lombok.extern.slf4j.Slf4j;
+//import org.openqa.selenium.WebDriver;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//
+//@Slf4j
+//@ContextConfiguration(classes = SpringConfig.class)
+//public class RichTextEditorPageTest extends AbstractTestNGSpringContextTests {
+//
+//    @Autowired
+//    private DriverFactory driverFactory;
+//
+//    private WebDriver driver;
+//
+//    private RichTextEditorPage richTextEditorPage;
+//
+//    @BeforeClass
+//    public void setup() {
+//        driver = driverFactory.createAndGetWebDriver();
+//        richTextEditorPage = new RichTextEditorPage(driver);
+//    }
+//
+//    @AfterClass
+//    public void tearDown() {
+//        log.info("Shutting down the driver.");
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//
+//    @Test
+//    public void testRichTextEditor() {
+//        // Given
+//        final String inputTextAutomation = "Automation ";
+//        final String inputTextTest = "Test";
+//        final String inputTextExample = " Example";
+//
+//        // When - Then
+//        richTextEditorPage.openPage()
+//                .toggleBold()
+//                .enterText(inputTextAutomation)
+//                .toggleBold()
+//                .toggleUnderline()
+//                .enterText(inputTextTest)
+//                .toggleUnderline()
+//                .enterText(inputTextExample)
+//                .assertTypedTextContainsBold(inputTextAutomation)
+//                .assertTypedTextContainsUnderlined(inputTextTest)
+//                .assertTypedTextContainsPlain(inputTextExample);
+//    }
+//}
